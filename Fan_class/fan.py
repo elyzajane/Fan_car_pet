7e984911e5b02a915b3fa3f4575dfc11ec9631ff
Elyza <elyzajanerelucio@gmail.com>
2023-07-01 16:54:23 +0800

    Create a class for fan

--- Fan_class/fan.py
+++ Fan_class/fan.py
@@ -1,6 +1,17 @@
 # This program defines a Fan class to represent a fan with methods to access and modify its speed, on/off state, radius, and color.
 
 # Create a class for fan
+class Fan:
+    SLOW = 1
+    MEDIUM = 2
+    FAST = 3
+
+    def __init__(self, speed=1, radius=5, color='blue', on=False):
+        self.__speed = speed
+        self.__on = on
+        self.__radius = radius
+        self.__color = color
+        
 # Get and set the speed
 # Set the fan on
 # Get and set the radius
