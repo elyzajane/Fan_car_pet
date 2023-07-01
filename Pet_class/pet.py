28c2fac31024b297a8f59abc4c16ac599e0d5568
Elyza <elyzajanerelucio@gmail.com>
2023-07-01 21:12:08 +0800

    Get and set the name, type and age of the animal

--- Pet_class/pet.py
+++ Pet_class/pet.py
@@ -6,7 +6,25 @@ class Pet:
         self.__name = ""
         self.__animal_type = ""
         self.__age = 0
-        
-# Get and set the name, type and age of the animal
+
+    # Get and set the name, type and age of the animal
+    def set_name(self, name):
+        self.__name = name
+
+    def set_animal_type(self, animal_type):
+        self.__animal_type = animal_type
+
+    def set_age(self, age):
+        self.__age = age
+
+    def get_name(self):
+        return self.__name
+
+    def get_animal_type(self):
+        return self.__animal_type
+
+    def get_age(self):
+        return self__age
+    
 # Submit and display the info given by the user
 # Run the main program
