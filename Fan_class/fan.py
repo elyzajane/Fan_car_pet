8ecbd5374e3b8a272e4120081765004a94abe336
Elyza <elyzajanerelucio@gmail.com>
2023-07-01 16:46:48 +0800

    Program outline for Fan class

---
+++ Fan_class/fan.py
@@ -0,0 +1,8 @@
+# This program defines a Fan class to represent a fan with methods to access and modify its speed, on/off state, radius, and color.
+
+# Create a class for fan
+# Get and set the speed
+# Set the fan on
+# Get and set the radius
+# Get and set the color
+# Run the program
