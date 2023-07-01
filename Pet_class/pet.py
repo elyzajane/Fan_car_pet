b162fd884a22354f7c5dbe1705f206e35edef8b2
Elyza <elyzajanerelucio@gmail.com>
2023-07-01 21:05:52 +0800

    Create a class for pet

--- Pet_class/pet.py
+++ Pet_class/pet.py
@@ -1,6 +1,12 @@
 # This program allows the user to input and store pet information (name, animal type, and age) and displays the saved data on the screen.
 
 # Create a class for pet
+class Pet:
+    def __init__(self):
+        self.__name = ""
+        self.__animal_type = ""
+        self.__age = 0
+        
 # Get and set the name, type and age of the animal
 # Submit and display the info given by the user
 # Run the main program
