bd0a2294eaf65ab10969e86f823df26e37357c67
Elyza <elyzajanerelucio@gmail.com>
2023-07-01 18:48:27 +0800

    Program outline for Car class

---
+++ Car_class/car.py
@@ -0,0 +1,6 @@
+# This program defines a simple car class that displays car information and control car speed with accelerate and brake buttons.
+
+# Create a class for Car
+# Use def functions to accelerate and brake to set the car's speed
+# Get the info of the car
+# Run the main program
