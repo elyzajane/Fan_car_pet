a7f868821a8dd46f0bc292ba36198383256baecd
Elyza <elyzajanerelucio@gmail.com>
2023-07-01 21:02:45 +0800

    Program outline for Pet class

---
+++ Pet_class/pet.py
@@ -0,0 +1,6 @@
+# This program allows the user to input and store pet information (name, animal type, and age) and displays the saved data on the screen.
+
+# Create a class for pet
+# Get and set the name, type and age of the animal
+# Submit and display the info given by the user
+# Run the main program
