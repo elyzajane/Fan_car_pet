2eb2ce8b8e596a423eb58f07d4b502f9179b7386
Elyza <elyzajanerelucio@gmail.com>
2023-07-01 21:26:58 +0800

    Import and create class for gui

--- Pet_class/pet.py
+++ Pet_class/pet.py
@@ -1,5 +1,8 @@
 # This program allows the user to input and store pet information (name, animal type, and age) and displays the saved data on the screen.
 
+# Import tkinter for designs
+import tkinter as tk
+
 # Create a class for pet
 class Pet:
     def __init__(self):
@@ -25,7 +28,38 @@ class Pet:
 
     def get_age(self):
         return self__age
-    
+
+# Create another class for gui
+class PetGUI:
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.title("Pet Information")
+        self.window.geometry("400x200")
+        self.window.configure(bg="#F0EAD6")
+
+        self.pet = Pet()
+
+        self.name_label = tk.Label(self.window, text="Name:", bg="#FFDAB9")
+        self.name_label.pack()
+
+        self.name_entry = tk.Entry(self.window, bg="#FFEFD5")
+        self.name_entry.pack()
+
+        self.type_label = tk.Label(self.window, text="Animal Type:", bg="#98FB98")
+        self.type_label.pack()
+
+        self.type_entry = tk.Entry(self.window, bg="#E0FFFF")
+        self.type_entry.pack()
+
+        self.age_label = tk.Label(self.window, text="Age:", bg="#B0C4DE")
+        self.age_label.pack()
+
+        self.age_entry = tk.Entry(self.window, bg="#FFC0CB")
+        self.age_entry.pack()
+
+        self.submit_button = tk.Button(self.window, text="Submit", command=self.submit_pet_info, bg="#87CEFA")
+        self.submit_button.pack(pady=10)
+            
     # Submit and display the info given by the user
     def submit_pet_info(self):
         name = self.name_entry.get()
